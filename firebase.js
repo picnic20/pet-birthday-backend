f3a2cf2ade99c1890db23963afc1b72e81082517
picnic20 <ohmygod112255@gmail.com>
2026-07-03 12:50:20 +0900

    last 10

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCoaW7ZDVGqsTqNjMItuuC6Drmii9wylpo",
+  authDomain: "stable-being-497702-i2.firebaseapp.com",
+  projectId: "stable-being-497702-i2",
+  storageBucket: "stable-being-497702-i2.firebasestorage.app",
+  messagingSenderId: "460437137026",
+  appId: "1:460437137026:web:065fe858287e8ea0a9369b",
+  measurementId: "G-07MJPC7N9L"
+};
